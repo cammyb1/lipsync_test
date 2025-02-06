bfd5129e7d2e035b966812488478116fc388a3dc
jvasquez <seiya_al_@hotmail.com>
2025-02-06 18:36:50 +0100

    feat: use skeletal animation as viseme input

--- src/main.js
+++ src/main.js
@@ -10,54 +10,143 @@ const { state, dispatcher } = mount3();
 
 // Visemes and morphtargets mapping
 export const VISEMES = {
-  A: "viseme_aa",
-  I: "viseme_I",
-  U: "viseme_U",
-  E: "viseme_E",
-  O: "viseme_O",
-  "-": "viseme_sil",
+  A: "FAnim_VisemA",
+  I: "FAnim_VisemI",
+  U: "FAnim_VisemU",
+  E: "FAnim_VisemE",
+  O: "FAnim_VisemO",
+  // "-": "FAnim_Blink",
 };
 
-function onAnimate() {
-  const character = state.scene.getObjectByName("rpm_character");
+async function main() {
+  let microphone = false;
+  let audio = null;
+  let context = new AudioContext();
+  let source = null;
+  let stream = null;
+  let processor = null;
+  let analyser = null;
 
-  if (!character) return;
+  const microButton = document.getElementById("micro");
+  const dropZone = document.getElementById("dropzone");
 
-  for (const key in lipSyncNode.weights) {
-    const viseme = VISEMES[key];
-    const value = lipSyncNode.weights[key] * lipSyncNode.volume;
+  function onAnimate() {
+    const character = state.scene.getObjectByName("rpm_character");
 
-    character.traverse((node) => {
-      if (node.isSkinnedMesh && node.morphTargetDictionary) {
-        const index = node.morphTargetDictionary[viseme];
-        node.morphTargetInfluences[index] = value;
+    if (!character) return;
+
+    for (const key in lipSyncNode.weights) {
+      const viseme = VISEMES[key];
+      const value = lipSyncNode.weights[key] * lipSyncNode.volume;
+      const action = state.actions[viseme];
+
+      if (action) {
+        action
+          .reset()
+          .setEffectiveWeight(1)
+          .setEffectiveTimeScale(value * 75);
       }
-    });
+      // With Morph Targets
+      // character.traverse((node) => {
+      //   if (node.isSkinnedMesh && node.morphTargetDictionary) {
+      //     const index = node.morphTargetDictionary[viseme];
+      //     node.morphTargetInfluences[index] = value;
+      //   }
+      // });
+    }
   }
-}
 
-async function connectAudio() {
-  const audioContext = new AudioContext();
-  lipSyncNode = await createWLipSyncNode(audioContext, profile);
+  async function connectAudio() {
+    lipSyncNode = await createWLipSyncNode(context, profile);
+    dispatcher.addEventListener("animate", onAnimate);
+  }
 
-  dispatcher.addEventListener("animate", onAnimate);
+  function stopStream() {
+    microButton.classList.remove("active");
 
-  window.addEventListener(
-    "click",
-    async (_) => {
-      if (audioContext.state === "suspended") {
-        audioContext.resume();
+    if (stream)
+      stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    if (analyser) analyser.disconnect();
+    if (source) source.disconnect();
+    if (processor) processor.disconnect();
+    if (audio) audio = null;
+  }
+
+  microButton.addEventListener("click", async () => {
+    microphone = !microphone;
+
+    if (microphone) {
+      if (context.state === "suspended") {
+        context.resume();
       }
 
-      const stream = await navigator.mediaDevices.getUserMedia({
+      microButton.classList.add("active");
+      stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
       });
-      const source = audioContext.createMediaStreamSource(stream);
+      source = context.createMediaStreamSource(stream);
 
       source.connect(lipSyncNode);
-    },
-    { passive: true, once: true }
-  );
+    } else if (source && stream) {
+      stopStream();
+    }
+  });
+
+  dropZone.addEventListener("drop", async (ev) => {
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          if (file.type === "audio/mpeg") {
+            microphone = false;
+            audio = file;
+          }
+        }
+      });
+    }
+
+    if (audio) {
+      source = context.createBufferSource();
+      const bufferStream = await audio.arrayBuffer();
+      source.buffer = await context.decodeAudioData(bufferStream);
+
+      processor = context.createScriptProcessor(2048, 1, 1);
+      processor.onaudioprocess = () => {
+        const freqData = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(freqData);
+      };
+      analyser = context.createAnalyser();
+
+      source.connect(context.destination);
+      source.connect(analyser);
+      source.connect(lipSyncNode);
+
+      analyser.connect(processor);
+      processor.connect(context.destination);
+
+      source.start();
+      setTimeout(disconnect, source.buffer.duration * 1000);
+    }
+  });
+
+  dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  await connectAudio();
 }
 
-await connectAudio();
+const enterB = document.getElementById("enter");
+
+enterB.onclick = async () => {
+  enterB.parentNode.classList.add("hidden");
+  setTimeout(() => {
+    enterB.parentNode.parentNode.removeChild(enterB.parentNode);
+  }, 1000);
+
+  await main();
+};
--- src/three.js
+++ src/three.js
@@ -6,12 +6,18 @@ import {
   WebGLRenderer,
   DirectionalLight,
   EventDispatcher,
+  AnimationMixer,
+  LoopOnce,
+  LoopRepeat,
+  Clock,
+  AdditiveAnimationBlendMode,
 } from "three";
 import { GLTFLoader, DRACOLoader } from "three/examples/jsm/Addons.js";
+import { makeClipAdditive } from "three/src/animation/AnimationUtils.js";
 
 const dispatcher = new EventDispatcher();
 
-function start({ scene, camera }) {
+function start({ scene, camera, actions, mixer }) {
   const ambient = new AmbientLight(0xffffff, 2.5);
   const directional = new DirectionalLight(0xffffff, 2.5);
   scene.add(ambient);
@@ -23,10 +29,23 @@ function start({ scene, camera }) {
   draco.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
   loader.setDRACOLoader(draco);
 
-  loader.load("./model.glb", (gltf) => {
+  loader.load("./visemes.glb", (gltf) => {
     const character = gltf.scene;
     character.name = "rpm_character";
-    scene.add(gltf.scene);
+    scene.add(character);
+
+    gltf.animations.forEach((anim) => {
+      const additiveClip = makeClipAdditive(anim, 0, anim, 30);
+      const action = mixer.clipAction(additiveClip, character);
+      action.blendMode = AdditiveAnimationBlendMode;
+      action.setLoop(LoopOnce);
+      action.setEffectiveWeight(0);
+      action.reset().play();
+      action.enabled = true;
+      actions[anim.name] = action;
+    });
+
+    actions["FAnim_Blink"].setEffectiveWeight(1).setLoop(LoopRepeat);
 
     const head = character.getObjectByName("Head");
     head.updateMatrixWorld();
@@ -46,8 +65,11 @@ export function mount3() {
     5
   );
   const scene = new Scene();
+  const mixer = new AnimationMixer();
+  const actions = {};
+  const clock = new Clock();
 
-  const state = { renderer, camera, scene };
+  const state = { renderer, camera, mixer, scene, actions };
 
   renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -57,8 +79,10 @@ export function mount3() {
     camera.updateProjectionMatrix();
   });
 
-  renderer.setAnimationLoop((dt) => {
+  renderer.setAnimationLoop(() => {
+    const dt = clock.getDelta();
     dispatcher.dispatchEvent({ type: "animate", state, dt });
+    mixer.update(dt);
     renderer.render(scene, camera);
   });
 
